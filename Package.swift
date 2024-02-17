fe35078e23f836859210235dedf402d49de473d3
Alex Fajkowski <alex@fajkowski.com>
2024-02-17 10:56:37 -0800

    Up Swift version to 5.9. Add support for visionOS. Remove testTarget and dependencies.

--- Package.swift
+++ Package.swift
@@ -1,29 +1,16 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SimpleKeychain",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
+    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7), .visionOS(.v1)],
     products: [.library(name: "SimpleKeychain", targets: ["SimpleKeychain"])],
-    dependencies: [
-        .package(url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "7.0.0")),
-        .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0")),
-    ],
     targets: [
         .target(
             name: "SimpleKeychain",
             dependencies: [],
             path: "SimpleKeychain",
             exclude: ["Info.plist"]),
-        .testTarget(
-            name: "SimpleKeychainTests",
-            dependencies: [
-                "SimpleKeychain",
-                .product(name: "Quick", package: "Quick"),
-                .product(name: "Nimble", package: "Nimble"),
-            ],
-            path: "SimpleKeychainTests",
-            exclude: ["Info.plist"])
     ]
 )
